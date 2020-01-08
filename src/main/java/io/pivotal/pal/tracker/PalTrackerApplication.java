6c147afe1b0d5acb73a83da7808d81653e981395
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:04 -0500

    commit all

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PalTrackerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
